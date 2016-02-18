7a70f70e6d297b1c04deb21ac04a4bd3947de3ab
Pankaj <pankaj@newgenray.com>
2016-02-18 06:06:20 +0530

    Version: 0.0.1 Toastr module. Only have include jquery or not as an option in system configuration

---
+++ app/code/local/Newgenray/Toastr/Block/Messages.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pankaj
+ * Date: 18-02-2016
+ * Time: 04:29
+ * Contact: For any type of assistance please send email to: pankaj@newgenray.com
+ */
+class Newgenray_Toastr_Block_Messages extends Mage_Core_Block_Messages
+{
+    /**
+     * Retrieve messages in HTML format grouped by type
+     *
+     * @param   string $type
+     * @return  string
+     */
+    public function getGroupedHtml()
+    {
+        $types = array(
+            Mage_Core_Model_Message::ERROR,
+            Mage_Core_Model_Message::WARNING,
+            Mage_Core_Model_Message::NOTICE,
+            Mage_Core_Model_Message::SUCCESS
+        );
+        $html = '';
+        foreach ($types as $type) {
+            if ( $messages = $this->getMessages($type) ) {
+                if ( !$html ) {
+                    $html .= '<script type="text/javascript">';
+                    $html .= 'jQuery(function($){';
+                    $html .= 'toastr.options = {
+                                "positionClass": "toast-bottom-left",
+                                "preventDuplicates": false
+                            };';
+                }
+                switch($type){
+                    case "success" : foreach ( $messages as $message ) {
+                        $msg = ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText();
+                        $html .= 'toastr.success("' . $msg . '");';
+                    }
+                        break;
+                    case "warning" : foreach ( $messages as $message ) {
+                        $msg = ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText();
+                        $html .= 'toastr.warning("' . $msg . '");';
+                        //$html .= 'toastr.warning(' . ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText() . ');';
+                    }
+                        break;
+                    case "notice" : foreach ( $messages as $message ) {
+                        $msg = ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText();
+                        $html .= 'toastr.info("' . $msg . '");';
+                        // $html .= 'toastr.info(' . ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText() . ');';
+                    }
+                        break;
+                    case "error" : foreach ( $messages as $message ) {
+                        $msg = ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText();
+                        $html .= 'toastr.error("' . $msg . '");';
+                        //$html .= 'toastr.error(' . ($this->_escapeMessageFlag) ? $this->escapeHtml($message->getText()) : $message->getText() . ');';
+                    }
+                        break;
+                }
+            }
+        }
+        if ( $html) {
+            $html .= '});';
+            $html .= '</script>';
+        }
+        return $html;
+    }
+}
---
+++ app/code/local/Newgenray/Toastr/Helper/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pankaj
+ * Date: 18-02-2016
+ * Time: 04:58
+ * Contact: For any type of assistance please send email to: pankaj@newgenray.com
+ */
+class Newgenray_Toastr_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
